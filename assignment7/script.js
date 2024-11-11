270bee2353b6d482cf0c91bbede7fdfcbb7bbe30
Nouh1408 <ahmedinouh@gmail.com>
2024-11-11 12:51:19 +0200

    solved three questions

---
+++ assignment7/script.js
@@ -0,0 +1,26 @@
+// Q1
+/* var x =prompt("Enter a number: ")
+alert(x) */
+
+//Q2
+/* var out= document.getElementById("demo")
+var x=prompt("Input: ")
+if(x%3===0 && x%4===0){
+    out.innerHTML ="Yes"
+}else{
+    out.innerHTML="No"
+}
+ */
+
+//Q3
+/* var n1 = prompt("enter number: ") //10
+var n2 = prompt("enter another number: ")//7
+
+n1 = Number(n1);
+n2 = Number(n2);
+
+if(n1>n2){
+    alert(n1)
+} else{
+    alert(n2)
+} */
