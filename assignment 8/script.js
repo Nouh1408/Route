3c52502b3de0a5746896e5134e8a737e92721e66
Nouh1408 <ahmedinouh@gmail.com>
2024-11-20 22:03:22 +0200

    done

--- assignment 8/script.js
+++ assignment 8/script.js
@@ -1,24 +1,45 @@
-function quotegenerator(){
+let lastIndex = -1;
+
+function quotegenerator() {
     var quotes = [
         {
-            quote:"“Be yourself; everyone else is already taken.”",
+            quote: "“Be yourself; everyone else is already taken.”",
             person: "― Oscar Wilde"
         },
         {
-            quote:"“So many books, so little time.”",
+            quote: "“So many books, so little time.”",
             person: "― Frank Zappa"
         },
         {
-            quote:"“A room without books is like a body without a soul.”",
-            person:"― Marcus Tullius Cicero"
-        }
-    ]
+            quote: "“A room without books is like a body without a soul.”",
+            person: "― Marcus Tullius Cicero"
+        },
+        {
+            quote: "“In three words I can sum up everything I've learned about life: it goes on.”",
+            person: "― Robert Frost"
+        },
+        {
+            quote: "“You only live once, but if you do it right, once is enough.”",
+            person: "― Mae West"
+        },
+        {
+            quote: "“If you tell the truth, you don't have to remember anything.”",
+            person: "― Mark Twain"
+        },
+    ];
+
+    let randomIndex;
+
+
+    do {
+        randomIndex = Math.floor(Math.random() * quotes.length);
+    } while (randomIndex === lastIndex);
+
+    lastIndex = randomIndex;
+
+    var randomQuote = quotes[randomIndex].quote;
+    var randomPerson = quotes[randomIndex].person;
+
+    document.getElementById("quoteoutput").innerText = randomQuote;
+    document.getElementById("name").innerText = randomPerson;
 }
-var randomIndex = Math.floor(Math.random() * quotes.length);
-var randomQuote = quotes[randomIndex].quote;
-var randomPerson = quotes[randomIndex].person;
-
-
-document.getElementById("quoteoutput").innerText=randomQuote
-document.getElementById("name").innerText=randomPerson
-
