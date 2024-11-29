a60f5078f45795e66c6241185ec90c4c1367a7c1
Nouh1408 <ahmedinouh@gmail.com>
2024-11-29 10:39:55 +0200

    sort of done

--- Assignment 9/Js/script.js
+++ Assignment 9/Js/script.js
@@ -1,21 +1,28 @@
 var bookmarkName = document.getElementById("bookmarkname")
 var bookmarkURL = document.getElementById("bookmarkurl")
-var sitelist=[]
-function addsite(){
-    book={
-        siteName:bookmarkName.value,
-        siteURL:bookmarkURL.value
+var sitelist = []
+function addsite() {
+    let book = {
+        siteName: bookmarkName.value,
+        siteURL: bookmarkURL.value
     }
     sitelist.push(book)
-    
+    displaySites()
 }
-function displaySites(){
-var cartona=''
-for(var i=0;i<sitelist.length;i++){
-    cartona +=`
-    
-    `
-}
+function displaySites() {
+    var cartona = ''
+    for (var i = 0; i < sitelist.length; i++) {
+        cartona += `
+    <tr>
+        <td>${i + 1}</td>
+        <td>${sitelist[i].siteName}</td>
+        <td><button class="btn btn-visit" onclick="visit('${sitelist[i].siteURL}')"><i class="fa-solid fa-eye"></i> Visit</button></td>
+        <td><button class="btn btn-del" onclick="deleteSite(${i})"><i class="fa-solid fa-trash-can"></i> Delete</button></td>
+</tr>`
+    }
 
-    document.getElementById("demo")
+    document.getElementById("content").innerHTML=cartona
+}
+function visit(url){
+    window.open(url,"_blamk")
 }
